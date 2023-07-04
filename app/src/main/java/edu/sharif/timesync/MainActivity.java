e68b5e6c80cb34e686b0285ca43b8e183ec0bf57
s0heil20 <soheil.mh2000@gmail.com>
2023-07-04 23:54:36 +0330

    add detailed group menu

--- app/src/main/java/edu/sharif/timesync/MainActivity.java
+++ app/src/main/java/edu/sharif/timesync/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import edu.sharif.timesync.groupDetailedMenu.GroupDetailedMenuActivity;
+import edu.sharif.timesync.groupDetailedMenu.GroupDetailedMenuAdapter;
 import edu.sharif.timesync.groupListMenu.GroupListMenuActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +15,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(this, GroupListMenuActivity.class));
+        startActivity(new Intent(this, GroupDetailedMenuActivity.class));
     }
 }
---
+++ app/src/main/java/edu/sharif/timesync/groupDetailedMenu/JobsFragment.java
@@ -0,0 +1,22 @@
+package edu.sharif.timesync.groupDetailedMenu;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.sharif.timesync.R;
+
+public class JobsFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_jobs, container, false);
+    }
+}
