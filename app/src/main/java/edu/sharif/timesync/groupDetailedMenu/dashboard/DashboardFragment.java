cf91cca128ce19290f7e7872f9d0871a0f98e3de
nedaei79 <nedaei79@gmail.com>
2023-07-05 14:30:00 +0330

    Added BarChart to Dashboard.

--- app/src/main/java/edu/sharif/timesync/groupDetailedMenu/dashboard/DashboardFragment.java
+++ app/src/main/java/edu/sharif/timesync/groupDetailedMenu/dashboard/DashboardFragment.java
@@ -1,21 +1,59 @@
 package edu.sharif.timesync.groupDetailedMenu.dashboard;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
 import edu.sharif.timesync.R;
 
 public class DashboardFragment extends Fragment {
 
+    private ArrayList<BarEntry> barArrayList;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dashboard, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+//        setting up
+        BarChart barChart = view.findViewById(R.id.barChart);
+        getData();
+        BarDataSet barDataSet = new BarDataSet(barArrayList, "fake");
+        BarData barData = new BarData(barDataSet);
+        barChart.setData(barData);
+
+//        setting data
+        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        barDataSet.setValueTextColor(Color.BLACK);
+        barDataSet.setValueTextSize(16f);
+    }
+
+    private void getData() {
+        barArrayList = new ArrayList<BarEntry>();
+        barArrayList.add(new BarEntry(2f, 10));
+        barArrayList.add(new BarEntry(3f, 20));
+        barArrayList.add(new BarEntry(4f, 30));
+        barArrayList.add(new BarEntry(5f, 40));
+        barArrayList.add(new BarEntry(6f, 50));
     }
 }
