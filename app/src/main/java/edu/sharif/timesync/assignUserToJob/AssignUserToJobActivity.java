3e3fe4fd779db3ad1f69888717e3346cb9a45eb6
s0heil20 <soheil.mh2000@gmail.com>
2023-07-05 13:25:38 +0330

    add AssignUserToJobActivity

--- app/src/main/java/edu/sharif/timesync/assignUserToJob/AssignUserToJobActivity.java
+++ app/src/main/java/edu/sharif/timesync/assignUserToJob/AssignUserToJobActivity.java
@@ -1,4 +1,47 @@
 package edu.sharif.timesync.assignUserToJob;
 
-public class AssignUserToJobActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.sharif.timesync.R;
+
+
+public class AssignUserToJobActivity extends AppCompatActivity {
+
+    private ListView usersList;
+    private ArrayAdapter<String> adapter;
+    private String[] usernames = {"user1", "user2"};
+    private Button submitButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assign_user_to_job);
+        usersList = findViewById(R.id.usersListViewAssignUserToJobMenu);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice,usernames);
+        usersList.setAdapter(adapter);
+        configureSubmitButton();
+
+    }
+
+    private void configureSubmitButton(){
+        submitButton = findViewById(R.id.submitButtonAssignUserToJobMenu);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String usersSelected = "Users selected: \n";
+                for (int i = 0;i < usersList.getCount();i++){
+                    if(usersList.isItemChecked(i)) {
+                        usersSelected += usersList.getItemAtPosition(i)+ " ";
+                    }
+                }
+                Toast.makeText(getBaseContext(), usersSelected, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
 }
